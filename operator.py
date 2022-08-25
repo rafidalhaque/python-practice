cfc2ca0360b01820f0bf091f131b07995a290ccd
rafidalhaque <68812773+rafidalhaque@users.noreply.github.com>
2022-08-25 17:28:09 +0600

    add operator.py

---
+++ operator.py
@@ -0,0 +1,20 @@
+a = int(input())
+b = int(input())
+
+# arithmetic operator
+
+suma = a + b
+sub = a - b
+multi = a * b
+div = a / b
+mod = a % b
+expo = a ** b
+floor = a // b
+
+print("addition is " + str(suma))
+print("subtraction is " + str(sub))
+print("multiplication is " + str(multi))
+print("division is " + str(div))
+print("modulas is " + str(mod))
+print("result is " + str(expo))
+print("result is " + str(floor)) 
